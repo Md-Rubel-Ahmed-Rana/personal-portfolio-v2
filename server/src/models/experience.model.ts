108e2f9d90a72aded5a83e1f1afaa27c74fe3b32
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-03-13 16:31:35 +0600

    Experience schema design done

---
+++ server/src/models/experience.model.ts
@@ -0,0 +1,48 @@
+import { Schema, model } from "mongoose";
+import { IExperience } from "../interfaces/experience.interface";
+
+const experienceSchema = new Schema<IExperience>({
+  name: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: Number,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  workLocation: {
+    type: String,
+    required: true,
+  },
+  designation: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+  website: {
+    type: String,
+  },
+  linkedIn: {
+    type: String,
+    required: true,
+  },
+  responsibilities: [String],
+  learnedNewTech: [String],
+});
+
+export const Experience = model<IExperience>("Experience", experienceSchema);
